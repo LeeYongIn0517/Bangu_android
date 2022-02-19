ddc53ae49b76e95afdf4fb41f02ea37fe4323dd0
Yoning Lee <0517lyi@naver.com>
2022-02-19 17:16:58 +0900

    Signup_Fin UI구현 및 xml 이름 정리

---
+++ app/src/main/java/com/example/bangu/login/ui/Adapter.kt
@@ -0,0 +1,4 @@
+package com.example.bangu.login.ui
+
+class Adapter {
+}
---
+++ app/src/main/java/com/example/bangu/login/ui/SignupFin_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.bangu.login.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupFin_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_fin)
+    }
+}
