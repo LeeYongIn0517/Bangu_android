65f97e15373f688fde672d2dd9179da5b9045c76
Yoning Lee <0517lyi@naver.com>
2022-02-23 17:35:17 +0900

    회원가입 id,pw 검사로직

--- app/src/main/java/com/example/bangu/signup/ui/SignupActivity.kt
+++ app/src/main/java/com/example/bangu/signup/ui/SignupActivity.kt
@@ -15,10 +15,16 @@ class SignupActivity : AppCompatActivity() {
         binding = ActivitySignupBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
+        //로그인
         binding.loginBtnpic.setOnClickListener{
             val next = Intent(this,SignupFinActivity::class.java)
             startActivity(next)
         }
+        //id, pw 조건검사 로직
+        //id는 영문,숫자,특수문자 조합 8-12자리
+
+        //pw는 영문,숫자,특수문자 조합 8-20자리
+
+        //pw 확인 로직도 필요함
     }
 }
