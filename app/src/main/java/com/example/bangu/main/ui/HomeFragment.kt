8ba053ba6b81234b9da26fe59cba0430e1fd122b
Yoning Lee <0517lyi@naver.com>
2022-02-28 22:50:56 +0900

    검색필터 프레그먼트 완성

--- app/src/main/java/com/example/bangu/main/ui/HomeFragment.kt
+++ app/src/main/java/com/example/bangu/main/ui/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.bangu.main.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,15 +12,16 @@ import com.example.bangu.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private val myHandler = Handler(Looper.getMainLooper())
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // 홈 프레그먼트 보여주기
-        binding = FragmentHomeBinding.inflate(inflater,container,false)
-        val view=binding.root
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
         return view
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,24 +31,33 @@ class HomeFragment : Fragment() {
     }
 
     override fun onStart() {
+        val workThread = Thread(HeightControl(550))
+        val workThread2 = Thread(HeightControl(0))
         super.onStart()
-        //EditText 누르면
-//        binding.searchBar.setOnClickListener{
-//            //검색필터 프레그먼트 호출하기
-//            val fragmentManager = childFragmentManager
-//            val fragmentTransaction = fragmentManager.beginTransaction()
-//            val fragment = SearchfilterFragment()
-//            fragmentTransaction.add(R.id.frame_filter, fragment)
-//            fragmentTransaction.commit()
-//        }
         binding.searchBar.setOnFocusChangeListener { view, b ->
-            if(b){
+            if (b) {
                 //검색필터 프레그먼트 호출하기
                 val fragmentManager = childFragmentManager
                 val fragmentTransaction = fragmentManager.beginTransaction()
                 val fragment = SearchfilterFragment()
                 fragmentTransaction.add(R.id.frame_filter, fragment)
                 fragmentTransaction.commit()
+                //검색필터 펼치기
+                workThread.start()
+            }
+            else{
+                //검색필터 접기
+                workThread2.start()
+            }
+        }
+    }
+    inner class HeightControl(i:Int):Runnable{
+        val h = i
+        override fun run() {
+            myHandler.post {
+                //필터 프레그먼트 화면 늘리기
+                binding.frameFilter.layoutParams.height = h
+                binding.frameFilter.invalidate()
             }
         }
     }
