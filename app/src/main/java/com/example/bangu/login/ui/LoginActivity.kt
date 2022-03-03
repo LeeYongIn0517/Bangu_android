5680e1632d4251d5a1fb418b5534b826d6e8c8a4
Yoning Lee <0517lyi@naver.com>
2022-03-04 06:25:57 +0900

    데이터바인딩 설정

--- app/src/main/java/com/example/bangu/login/ui/LoginActivity.kt
+++ app/src/main/java/com/example/bangu/login/ui/LoginActivity.kt
@@ -3,6 +3,9 @@ package com.example.bangu.login.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.bangu.R
 import com.example.bangu.databinding.ActivityLoginBinding
 import com.example.bangu.login.data.LgRepository
@@ -11,26 +14,35 @@ import com.example.bangu.signup.ui.SignupActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    var error = R.drawable.login_input
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
         setContentView(binding.root)
 
-//        var id = (binding.loginId.toString()).toLongOrNull() //숫자가 아니면 null을 반환
-//        var pw = (binding.loginPw.toString()).toCharArray()
-//        //id, pw 조건 검사 필요
-//
+        var id = (binding.loginId.toString()).toLongOrNull() //숫자가 아니면 null을 반환
+        var pw = (binding.loginPw.toString()).toCharArray()
+        binding.loginId.setBackgroundResource(R.drawable.login_input)
+        //앱 자체 로그인인증 시작
+        binding.loginStartbtn.setOnClickListener{
+            //로그인 실패할 경우 빨간 입력란으로 경고
+            error = R.drawable.login_input_error //glide로 할 필요는 없을까?
+
+            //로그인 성공할 경우
+            val next = Intent(this, MainActivity::class.java)
+            startActivity(next)
+            binding.invalidateAll()
+        }
+
 //        //Call로 받은 응답-> LgDataResource의 응답인터페이스로 성공, 실패 상황 구현하기
 //        val lgRepo = LgRepository
 //        lgRepo.requestLogin()
+
+        //회원가입 페이지로
         binding.loginSignUp.setOnClickListener{
             val next = Intent(this,SignupActivity::class.java)
             startActivity(next)
         }
-        binding.loginBtnpic.setOnClickListener{
-            val next = Intent(this, MainActivity::class.java)
-            startActivity(next)
-        }
-
     }
 }
