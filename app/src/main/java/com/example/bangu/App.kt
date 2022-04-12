1bdfe108122ea8ffc1eac78a1b555d0b93949f84
Yoning Lee <0517lyi@naver.com>
2022-04-12 17:00:09 +0900

    concrypt 추가

--- app/src/main/java/com/example/bangu/App.kt
+++ app/src/main/java/com/example/bangu/App.kt
@@ -3,6 +3,8 @@ package com.example.bangu
 import android.app.Application
 import com.example.bangu.login.data.TokenSharedPreferences
 import com.example.bangu.signup.data.model.SignupSharedPreferences
+import org.conscrypt.Conscrypt
+import java.security.Security
 
 class App:Application() {
     companion object{
@@ -13,6 +15,7 @@ class App:Application() {
     override fun onCreate() {
         token_prefs = TokenSharedPreferences(applicationContext)
         signup_prefs = SignupSharedPreferences(applicationContext)
+        Security.insertProviderAt(Conscrypt.newProvider(),1)
         super.onCreate()
     }
 }
