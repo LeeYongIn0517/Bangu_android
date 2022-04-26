ada93e811b8b510afb1b75e05faf120e632dfb2d
Yoning Lee <0517lyi@naver.com>
2022-04-26 15:36:52 +0900

    Tablayout, ViewPager 프레그먼드4개 생성

--- app/src/main/java/com/example/bangu/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/bangu/main/ui/MainActivity.kt
@@ -1,22 +1,54 @@
 package com.example.bangu.main.ui
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.widget.ViewPager2
+import com.example.bangu.R
 import com.example.bangu.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : FragmentActivity() {
     private lateinit var binding:ActivityMainBinding
     private lateinit var viewPager: ViewPager2
+    private lateinit var tablayout: TabLayout
+    private lateinit var tabIcon0:View
+    private lateinit var tabIcon1:View
+    private lateinit var tabIcon2:View
+    private lateinit var tabIcon3:View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        tablayout = binding.mainBtmbar
 
         //ViewPager2 등록
         viewPager = binding.viewPager
         val sspAdapter = ScreenSlidePagerAdapter(this)
         viewPager.adapter = sspAdapter
+
+        val tabIcons = arrayListOf<Any>(4,{})
+
+        TabLayoutMediator(tablayout,viewPager, TabLayoutMediator.TabConfigurationStrategy{tab: TabLayout.Tab, position: Int ->
+            when(position){
+                0->tab.apply {
+                    setIcon(R.drawable.main_home_states)
+                    setText("홈")}
+                1->tab.apply {
+                    setIcon(R.drawable.main_mybangu_states)
+                    setText("내 방구석")}
+                2->tab.apply{
+                    setIcon(R.drawable.main_feed_states)
+                    setText("피드")}
+                3->tab.apply {
+                    setIcon(R.drawable.main_profile_states)
+                    setText("프로필")}
+            }
+        }).attach()
+
     }
     override fun onBackPressed() {
         if (viewPager.currentItem == 0) {
--- app/src/main/java/com/example/bangu/main/ui/ScreenSlidePagerAdapter.kt
+++ app/src/main/java/com/example/bangu/main/ui/ScreenSlidePagerAdapter.kt
@@ -3,11 +3,22 @@ package com.example.bangu.main.ui
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.bangu.main.home.ui.HomeRvFragment
+import com.example.bangu.main.feed.ui.FeedFragment
+import com.example.bangu.main.home.ui.HomeFragment
+import com.example.bangu.main.mybangu.ui.MyBanguFragment
+import com.example.bangu.main.profile.ui.ProfileFragment
 
-private const val NUM_PAGES = 1
+private const val NUM_PAGES = 4
+
 class ScreenSlidePagerAdapter(fa: FragmentActivity): FragmentStateAdapter(fa){
-    override fun createFragment(position: Int): Fragment = HomeRvFragment()
 
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> HomeFragment()
+            1 -> MyBanguFragment()
+            2 -> FeedFragment()
+            else -> {ProfileFragment()} //position = 3
+        }
+    }
     override fun getItemCount(): Int = NUM_PAGES
 }
