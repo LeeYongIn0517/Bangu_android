ccce72c1cbde7a7732ff036df8a2cccfdff945dc
Yoning Lee <0517lyi@naver.com>
2022-06-18 22:21:10 +0900

    Fix: "영화제목 줄바꿈 기능 수정"
    ott아이콘 개수에 따라 엔터삽입 기준 길이를 달리 함

--- app/src/main/java/com/example/bangu/main/mybangu/ui/SearchPuAdapter.kt
+++ app/src/main/java/com/example/bangu/main/mybangu/ui/SearchPuAdapter.kt
@@ -40,14 +40,14 @@ class SearchPuAdapter(private val listener:Communicator):RecyclerView.Adapter<Re
                     when(movieData.movieOtts!!.get(i).ottName){
                         "NETFLIX" -> binding.resultNetflix.visibility = View.VISIBLE
                         "TVING" -> binding.resultTving.visibility = View.VISIBLE
-                        "WATCHA" -> binding.resultWatcha.visibility = View.VISIBLE
+                        "WATCHAPLAY" -> binding.resultWatcha.visibility = View.VISIBLE
                         "WAVVE" -> binding.resultWavve.visibility = View.VISIBLE
                     }
                 }
                 resultGenre.text = movieData.genre //영화 장르
                 resultDirector.text = movieData.director //영화 감독
                 resultActor.text = movieData.actor //영화 배우
-                resultTitle.text = setEnter(movieData.title) //영화 제목
+                resultTitle.text = setEnter(movieData.title,ottSize) //영화 제목
             }
             /*영화 작품 선택 시*/
             binding.movieItem.setOnClickListener {
@@ -102,17 +102,24 @@ class SearchPuAdapter(private val listener:Communicator):RecyclerView.Adapter<Re
         //items.add(MovieResponseData(" ",null," "," "," "," ")) //마지막에 빈 아이템 추가(로딩 뷰)
     }
     /*아이템의 영화제목 길이에 따라 엔터를 삽입하는 함수*/
-    fun setEnter(title:String):String{
-        val item_title = StringBuilder().append(title)
-        if(title.length > 14){
-            val mString = title.split(" ").toTypedArray()
-            var sum = 0
+    fun setEnter(title:String,ottSize:Int):String{
+        val item_title = StringBuilder().append(title) //엔터 삽입 후 리턴될 제목
+        if(title.length > 12){
+            val mString = title.split(" ").toTypedArray() //띄어쓰기로 원소 나누기
+            var sum = 0 //엔터 삽입 위치 카운팅
             for(i in 0 until mString.size){
                 sum += mString[i].length + 1
-                if(sum > 10) break //엔터 삽입 위치 찾음
+                when(ottSize){
+                    //ott개수에 따라서 엔터 삽입위치가 달라짐
+                    1->if(sum > 16) break
+                    2->if(sum > 14) break
+                    3->if(sum > 12) break
+                    4->if(sum > 10) break
+                }
             }
             item_title.insert(sum-1,"\n")
         }
+
         return item_title.toString()
     }
 
