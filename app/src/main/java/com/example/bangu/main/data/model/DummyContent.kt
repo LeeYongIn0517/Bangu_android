5dcd76c68227c1c4fa4a9c0fe1a0551b9fb81a5f
Yoning Lee <0517lyi@naver.com>
2022-08-19 00:27:55 +0900

    Feat: "생성 - 더미데이터"
    개발 중 서버 불안정할 때 UI상에 확인할 게 있어서 잠깐 만들다 말았다.
    삭제할까 말까 고민했는데 일단 놔두기로 함.

---
+++ app/src/main/java/com/example/bangu/main/data/model/DummyContent.kt
@@ -0,0 +1,41 @@
+package com.example.bangu.main.data.model
+//
+//import com.google.gson.annotations.SerializedName
+//
+//data class DummyContent(
+//    var id: Int = 0,
+//    var userProfileData:UserProfileData2?,
+//    var movieResponseData:MovieResponseData2?,
+//    var followState: Boolean = false,
+//    var loginUser:Boolean = false,
+//    var reviewOttResponseData:List<ReviewOttResponseData2>?,
+//    var score: Float = 3.5f,
+//    var attention:String = "이것은 더미데이터 몰라용 명대사",
+//    var content:String = "이것은 더미데이터 몰라용 이것은 더미데이터 몰라용 이것은 더미데이터 몰라용 이것은 더미데이터 몰라용 이것은 더미데이터 몰라용",
+//    var bookmark:Boolean = true
+//)
+//data class UserProfileData2(
+//    var id:Int = 0,
+//    var nickname:String = "더미데이터",
+//    var imageUrl:String = "",
+//    var gender:String = "F",
+//    var birth:Int = 20010517
+//)
+//data class MovieResponseData2(
+//    var title:String = "아이언맨",
+//    var movieOtts:List<MovieOtts>?,
+//    @SerializedName("imageUrl")
+//    var imageUrl:String,
+//    @SerializedName("genre")
+//    var genre:String,
+//    @SerializedName("director")
+//    var director:String?,
+//    @SerializedName("actor")
+//    var actor:String?,
+//):java.io.Serializable
+//data class ReviewOttResponseData2(
+//    @SerializedName("id")
+//    var id:Int,
+//    @SerializedName("ottName")
+//    var ottName:String
+//)
