99dde671011706f4af7c02ac9718d029cc9ad992
Yoning Lee <0517lyi@naver.com>
2022-09-01 00:36:45 +0900

    Feat:"추가 - 팔로잉 리사이클뷰 데이터요청 "

--- app/src/main/java/com/example/bangu/main/profile/ui/FollowingFragment.kt
+++ app/src/main/java/com/example/bangu/main/profile/ui/FollowingFragment.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.bangu.R
 import com.example.bangu.databinding.FragmentFollowingBinding
+import com.example.bangu.main.profile.presentation.FollowingViewModel
+import com.example.bangu.main.profile.presentation.ProfileViewModel
+import io.reactivex.disposables.CompositeDisposable
 
 class FollowingFragment: Fragment() {
     private lateinit var binding: FragmentFollowingBinding
+    private var page = 0
+    private val ITEMS_SIZE = 10
+    private val disposables = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +29,38 @@ class FollowingFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentFollowingBinding.inflate(inflater,container,false)
+        val viewmodel = FollowingViewModel()
+        val adapter = FollowingAdapter()
+
+        /**어댑터 등록*/
+        binding.recyclerview.adapter = adapter
+        /**서버에 데이터 초기요청 1번*/
+        if(page == 0) viewmodel.requestFollowing(page,ITEMS_SIZE,disposables) /**사용자가 팔로잉하는 유저 조회*/
+        /**스크롤 리스너*/
+        binding.recyclerview.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val lastVisibleItemPosition = (recyclerView.layoutManager as LinearLayoutManager) !!.findLastCompletelyVisibleItemPosition()
+                val itemTotalCount = recyclerView.adapter!!.itemCount - 1
+
+                /**스크롤이 끝에 도달했는지 확인*/
+                if(!binding.recyclerview.canScrollVertically(1) && lastVisibleItemPosition == itemTotalCount){
+                    viewmodel.requestFollowing(++page,ITEMS_SIZE,disposables)
+                }
+            }
+        })
 
         /**백 버튼*/
-        binding.btnBack.setOnClickListener{
+        binding.btnBack.setOnClickListener {
             //프로필 페이지로 돌아가기
-            parentFragmentManager.beginTransaction(). replace(R.id.profile_root_frag, ProfileFragment()).commit()
+            parentFragmentManager.beginTransaction().replace(R.id.profile_root_frag, ProfileFragment()).commit()
         }
         return binding.root
+    }
+
+    override fun onStop() {
+        super.onStop()
+        //관리하고 있던 디스포저블 객체를 모두 해제
+        disposables.clear()
     }
 }
