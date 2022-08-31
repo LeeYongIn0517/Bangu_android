59f894591258fa005a7bf380337bde8bd3db8311
Yoning Lee <0517lyi@naver.com>
2022-09-01 00:33:05 +0900

    Feat:"생성 - 팔로워/팔로잉 페이지 리사이클뷰 바인딩"

---
+++ app/src/main/java/com/example/bangu/main/profile/presentation/FollowingViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.bangu.main.profile.presentation
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.example.bangu.App
+import com.example.bangu.main.profile.data.ProfileDataResource
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+
+class FollowingViewModel:ViewModel() {
+    private val profileService = ProfileDataResource.ProfileApi
+    val accessToken = App.token_prefs.accessToken
+    val id = App.signup_prefs.sp_id //사용자 식별자
+
+    fun requestFollowing(page:Int,size:Int,disposables: CompositeDisposable){
+        if (accessToken != null) {
+            disposables.add(
+                profileService.requestFollowing(accessToken,id,page,size)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        Log.d("FollowingViewModel","requestFollowing().success")
+                    }){
+                        Log.d("FollowingViewModel","requestFollowing().fail")
+                    }
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bangu/main/profile/ui/FollowingAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.bangu.main.profile.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.Target
+import com.example.bangu.R
+import com.example.bangu.databinding.ItemFollowingBinding
+import com.example.bangu.main.profile.data.model.FollowContent
+
+class FollowingAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val items = ArrayList<FollowContent>()
+
+    inner class ItemViewHolder(private val binding: ItemFollowingBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(content:FollowContent){
+            /**유저닉네임, 이미지 바인딩*/
+            binding.apply {
+                nickname.text = content.nickname
+                when(content.imageUrl){
+                    "" -> Glide.with(binding.root).load(R.drawable.review_icon).override(Target.SIZE_ORIGINAL).into(binding.image)
+                    else -> Glide.with(binding.root).load(content.imageUrl).override(Target.SIZE_ORIGINAL).into(binding.image)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemFollowingBinding.inflate(layoutInflater, parent, false)
+
+        return ItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder is ItemViewHolder) holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
