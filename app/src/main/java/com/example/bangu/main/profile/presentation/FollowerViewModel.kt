52e83bcaadfa71e64a86d901ac5e9274852bd0c3
Yoning Lee <0517lyi@naver.com>
2022-09-01 02:08:31 +0900

    Feat:"추가 - 팔로워/팔로잉 데이터방출 스레드"

--- app/src/main/java/com/example/bangu/main/profile/presentation/FollowerViewModel.kt
+++ app/src/main/java/com/example/bangu/main/profile/presentation/FollowerViewModel.kt
@@ -6,6 +6,7 @@ import com.example.bangu.App
 import com.example.bangu.main.profile.data.ProfileDataResource
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class FollowerViewModel:ViewModel() {
     private val profileService = ProfileDataResource.ProfileApi
@@ -16,6 +17,7 @@ class FollowerViewModel:ViewModel() {
         if (accessToken != null) {
             disposables.add(
                 profileService.requestFollower(accessToken, id, page, size)
+                    .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe({
                         Log.d("FollowingViewModel", "requestFollower().success")
--- app/src/main/java/com/example/bangu/main/profile/presentation/FollowingViewModel.kt
+++ app/src/main/java/com/example/bangu/main/profile/presentation/FollowingViewModel.kt
@@ -6,6 +6,7 @@ import com.example.bangu.App
 import com.example.bangu.main.profile.data.ProfileDataResource
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class FollowingViewModel:ViewModel() {
     private val profileService = ProfileDataResource.ProfileApi
@@ -16,6 +17,7 @@ class FollowingViewModel:ViewModel() {
         if (accessToken != null) {
             disposables.add(
                 profileService.requestFollowing(accessToken,id,page,size)
+                    .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe({
                         Log.d("FollowingViewModel","requestFollowing().success")
