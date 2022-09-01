0991b8fbfc4bc497ed87b79e882709c6e1aa2b5f
Yoning Lee <0517lyi@naver.com>
2022-09-01 15:56:34 +0900

    Renmae:"수정 - xml파일 상 명칭변경에 따른 수정"

--- app/src/main/java/com/example/bangu/main/home/ui/HomeFragment.kt
+++ app/src/main/java/com/example/bangu/main/home/ui/HomeFragment.kt
@@ -23,6 +23,7 @@ class HomeFragment : Fragment() {
     private var page = 0
     private val ITEMS_SIZE = 10
     private val TYPE_REVIEW = "home"
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,7 +41,7 @@ class HomeFragment : Fragment() {
         val adapter = HomeAdapter()
 
         //어댑터 등록
-        binding.homeRcycleview
+        binding.homeRecyclerview
             .adapter = adapter
         //서버에서 온 데이터를 관찰하는 옵저버
         viewmodel.reviewList.observe(viewLifecycleOwner, Observer {
@@ -52,7 +53,7 @@ class HomeFragment : Fragment() {
             viewmodel.requestReviewList(page,ITEMS_SIZE,TYPE_REVIEW)
         }
         //스크롤 리스너
-        binding.homeRcycleview.addOnScrollListener(object: RecyclerView.OnScrollListener(){
+        binding.homeRecyclerview.addOnScrollListener(object: RecyclerView.OnScrollListener(){
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
 
@@ -60,7 +61,7 @@ class HomeFragment : Fragment() {
                 val itemTotalCount = recyclerView.adapter!!.itemCount - 1
 
                 //스크롤이 끝에 도달했는지 확인
-                if(!binding.homeRcycleview.canScrollVertically(1) && lastVisibleItemPosition == itemTotalCount){
+                if(!binding.homeRecyclerview.canScrollVertically(1) && lastVisibleItemPosition == itemTotalCount){
                     viewmodel.requestReviewList(++page, ITEMS_SIZE,TYPE_REVIEW)
                 }
             }
@@ -80,42 +81,9 @@ class HomeFragment : Fragment() {
                 }
             }
         })
+        /**검색기능*/
+
     }
 
-    override fun onStart() {
-        super.onStart()
-        val workThread = Thread(HeightControl(550)) //검색필터 펼치기
-        val workThread2 = Thread(HeightControl(0)) //검색필터 접기
-        //검색필터 바를 통해 검색필터 열고닫기
-        binding.searchbar.setOnFocusChangeListener { view, b ->
-            if (b) {
-                //검색필터 프레그먼트 호출하기
-                val fragmentManager = childFragmentManager
-                val fragmentTransaction = fragmentManager.beginTransaction()
-                val fragment = SearchfilterFragment()
-                fragmentTransaction.add(R.id.frame_filter, fragment)
-                fragmentTransaction.commit()
-                //검색필터 펼치기
-                workThread.start()
-            }
-            else{
-                //검색필터 접기
-                workThread2.start()
-            }
-        }
-        binding.searchIcon.setOnClickListener {
-            //검색결과 data단으로 넘겨주기
-        }
-    }
-    inner class HeightControl(i:Int):Runnable{
-        val h = i
-        override fun run() {
-            myHandler.post {
-                //필터 프레그먼트 화면 늘리기
-                binding.frameFilter.layoutParams.height = h
-                binding.frameFilter.invalidate()
-            }
-        }
-    }
 }
 
