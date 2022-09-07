ed1de74497efe8fa344c902cfac90f15a01b3969
Yoning Lee <0517lyi@naver.com>
2022-09-07 13:32:26 +0900

    Fix:"수정 - 팔로잉 리뷰리스트 어댑터 "
    -observeOn 스레드 다시 지정(작업스레드 -> 메인스레드)
    -장르 데이터 non-null 추가
    -onBindViewHolder 홀더 다시 지정(home -> feed)

--- app/src/main/java/com/example/bangu/main/feed/ui/FeedAdapter.kt
+++ app/src/main/java/com/example/bangu/main/feed/ui/FeedAdapter.kt
@@ -3,25 +3,23 @@ package com.example.bangu.main.feed.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.bangu.R
 import com.example.bangu.databinding.ItemReviewBinding
 import com.example.bangu.main.data.model.Content
-import com.example.bangu.main.home.ui.HomeAdapter
 
 class FeedAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val items = ArrayList<Content>()
     private val viewmodel = FeedViewModel()
 
     inner class ReviewViewHolder(private val binding:ItemReviewBinding):RecyclerView.ViewHolder(binding.root){
-        //장르 해쉬태그 (최대 6개)
+        /**장르 해쉬태그 (최대 6개)*/
         private val genre_array = arrayListOf<TextView>(
             binding.defaultGenre0,binding.defaultGenre1,binding.defaultGenre2,
             binding.defaultGenre3,binding.defaultGenre4,binding.defaultGenre5)
-        //별점 이미지
+
         fun bind(content:Content){
             //북마크 추가, 삭제 기능
             var sign:Boolean
@@ -33,7 +31,7 @@ class FeedAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                     viewmodel.adjustBookmark(content.id)
                 }
             }
-            //리뷰를 작성한 계정 프로필 바인딩
+            /**리뷰를 작성한 계정 프로필 바인딩*/
             binding.apply {
                 userNickname.text = content.userProfileData?.nickname
                 userGender.text = if(content.userProfileData?.gender == "M") "남성" else "여성"
@@ -46,32 +44,34 @@ class FeedAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                     else ->  "60대 이상"
                 }
             }
-            //프로필 이미지 바인딩
+            /**프로필 이미지 바인딩*/
             val profile_imageUrl = content.userProfileData?.imageUrl
             when(profile_imageUrl){
                 "" -> Glide.with(binding.root).load(R.drawable.review_icon).override(28,28).into(binding.userImage)
                 else -> Glide.with(binding.root).load(profile_imageUrl).override(28,28).into(binding.userImage)
             }
-            //영화 및 리뷰 내용, 팔로잉, 북마크 바인딩
+            /**영화 및 리뷰 내용, 팔로잉, 북마크 바인딩*/
             binding.apply {
                 reviewMovietitle.text = content.movieResponseData?.title
                 reviewCore.text = content.dialogue //영화 내용
                 reviewBookmark.isSelected = content.bookmark //영화 북마크 여부
                 //영화 장르 해쉬 태그
-                var genre_token = content.movieResponseData!!.genre.split(' ')
-                for(i in 0..genre_token.size){
-                    genre_array[i].text = genre_token[i] //장르 문자열 삽입
-                    genre_array[i].visibility = View.VISIBLE //가시적이게 전환
+                var genre_token = content.movieResponseData?.genre?.split(' ')
+                if (genre_token != null) {
+                    for(i in 0..genre_token.size-1){
+                        genre_array[i].text = genre_token[i] //장르 문자열 삽입
+                        genre_array[i].visibility = View.VISIBLE //가시적이게 전환
+                    }
                 }
             }
-            //영화 이미지 바인딩
+            /**영화 이미지 바인딩*/
             val movie_imageUrl = content.movieResponseData?.imageUrl
             when(movie_imageUrl){
                 " " -> Glide.with(binding.root).load(R.drawable.movie01).override(127,100).into(binding.reviewMovieimage)
                 null -> Glide.with(binding.root).load(R.drawable.movie03).override(127,100).into(binding.reviewMovieimage)
                 else -> Glide.with(binding.root).load(movie_imageUrl).override(127,100).into(binding.reviewMovieimage)
             }
-            //OTT 바인딩
+            /**OTT 바인딩*/
             val ottSize = content.reviewOttResponseData?.size
             for(i in 0 until ottSize!!){
                 when(content.reviewOttResponseData!![i].ottName){
@@ -81,7 +81,7 @@ class FeedAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                     "WAVVE" -> binding.wavve.visibility = View.INVISIBLE
                 }
             }
-            //피드 페이지의 리뷰이기 때문에 팔로우 버튼 invisible처리
+            /**피드 페이지의 리뷰이기 때문에 팔로우 버튼 invisible처리*/
             binding.followingBtn.visibility = View.INVISIBLE
         }
     }
@@ -94,7 +94,7 @@ class FeedAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if(holder is HomeAdapter.ReviewViewHolder){
+        if(holder is ReviewViewHolder){
             holder.bind(items[position])
         }
     }
