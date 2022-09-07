bafbd5c1a4862b51801e6347d13b2c5e42b66cb6
Yoning Lee <0517lyi@naver.com>
2022-09-07 14:14:30 +0900

    Feat:"추가 - 피드페이지 작품검색(영화 및 리뷰)"

--- app/src/main/java/com/example/bangu/main/feed/ui/FeedFragment.kt
+++ app/src/main/java/com/example/bangu/main/feed/ui/FeedFragment.kt
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bangu.databinding.FragmentFeedBinding
 import com.example.bangu.main.data.model.Content
+import com.example.bangu.main.data.model.MovieResponseData
 import com.example.bangu.main.feed.ui.FeedAdapter
 import com.example.bangu.main.feed.ui.FeedViewModel
 import io.reactivex.disposables.CompositeDisposable
@@ -19,8 +20,10 @@ import io.reactivex.disposables.CompositeDisposable
 class FeedFragment : Fragment() {
     private lateinit var binding: FragmentFeedBinding
     private var page = 0
+    private var pageMovie = 0
     private val ITEMS_SIZE = 20
     private val TYPE_REVIEW = "following"
+    private val sortType = false
     private val disposables =  CompositeDisposable() //같은 모듈 안에서만 볼 수 있음
 
     override fun onCreateView(
@@ -48,7 +51,7 @@ class FeedFragment : Fragment() {
         if(page == 0){
             viewmodel.requestReviewList(page,ITEMS_SIZE,TYPE_REVIEW,disposables)
         }
-        
+
         /**리뷰데이터 옵저버*/
         viewmodel.reviewList.observe(viewLifecycleOwner, Observer {
             adapter.setList(it as MutableList<Content>)
@@ -71,14 +74,36 @@ class FeedFragment : Fragment() {
         })
 
         /**검색 기능*/
-
+        binding.feedIcSearch.setOnClickListener {
+            viewmodel.searchMovie(binding.feedSearch.text.toString(),pageMovie,ITEMS_SIZE,disposables)
+            viewmodel.searchReviews(binding.feedSearch.text.toString(),sortType,disposables)
+        }
         /**영화 검색결과 옵저버*/
-
+        viewmodel.movieList.observe(viewLifecycleOwner, Observer {
+            /**페이지 타이틀('팔로잉') 지우기*/
+            binding.feedTitle.visibility = View.GONE
+            /**feed_recyclerview 가시화, 바인딩*/
+            binding.feedMovieRecyclerview.visibility = View.VISIBLE
+            movieAdapter.setList(it as MutableList<MovieResponseData>)
+            movieAdapter.notifyItemRangeInserted(page*ITEMS_SIZE,ITEMS_SIZE)
+        })
+        /**리뷰 검색결과 옵저버*/
+        viewmodel.searchReviews.observe(viewLifecycleOwner, Observer {
+            /**리사이클뷰 초기화하고 검색결과 내용으로 바꾸기*/
+            adapter.clearList()
+            adapter.apply {
+                setList(it as MutableList<Content>)
+                notifyItemRangeInserted(0,adapter.itemCount)
+            }
+        })
         /**백 버튼 - '팔로잉' 화면으로 돌아가기*/
         binding.feedBack.setOnClickListener {
             /**페이지 타이틀('팔로잉') 초기화*/
             binding.feedTitle.visibility = View.VISIBLE
             /**영화 검색결과 리스트 초기화*/
+            movieAdapter.clearList()
+            binding.feedMovieRecyclerview.visibility = View.GONE
+            pageMovie = 0
             /**리사이클뷰 초기화*/
             adapter.clearList()
             /**최근 올라온 리뷰의 데이터를 요청하도록 페이지 초기화*/
--- app/src/main/java/com/example/bangu/main/feed/ui/FeedViewModel.kt
+++ app/src/main/java/com/example/bangu/main/feed/ui/FeedViewModel.kt
@@ -9,6 +9,7 @@ import com.example.bangu.Event
 import com.example.bangu.login.data.LgDataResource
 import com.example.bangu.main.data.MainDataResource
 import com.example.bangu.main.data.model.Content
+import com.example.bangu.main.data.model.MovieResponseData
 import com.example.bangu.main.feed.data.FeedDataResource
 import com.example.bangu.main.feed.data.FeedRepository
 import io.reactivex.Observable
@@ -27,25 +28,56 @@ class FeedViewModel: ViewModel() {
     val reviewList: LiveData<List<Content>> = _reviewList
     private var _BookMark = MutableLiveData<Event<String>>()
     val BookMark: LiveData<Event<String>> = _BookMark
-
+    private var _movieList = MutableLiveData<List<MovieResponseData>>()
+    val movieList:LiveData<List<MovieResponseData>> = _movieList
+    private var _searchReviews = MutableLiveData<List<Content>>()
+    val searchReviews: LiveData<List<Content>> = _searchReviews
     //팔로잉하는 리뷰어의 리뷰리스트 요청
     val accessToken = App.token_prefs.accessToken
-    fun requestReviewList(page:Int, size:Int,type:String,disposables: CompositeDisposable){
-        if (accessToken != null) {
-            Log.d("accessToken",accessToken)
-        }
-        Log.d("FeedVM","requestReviewList")
 
+    fun requestReviewList(page:Int, size:Int,type:String,disposables: CompositeDisposable){
         if (accessToken != null) {
             disposables.add(
                 MainService.requestReviewList(accessToken,page,size,type)
                     .subscribeOn(Schedulers.io())
-                    .observeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
                     .subscribe({
                         Log.d("FeedViewModel","requestReviewList().success")
                         _reviewList.value = it.content
                     }){
                         Log.d("FeedViewModel","requestReviewList().fail")
+                    }
+            )
+        }
+    }
+    /**영화 이름으로 작품 검색*/
+    fun searchMovie(name:String, page: Int, size: Int,disposable: CompositeDisposable){
+        if(accessToken != null) {
+            disposable.add(
+                MainService.searchMovies(name, page, size)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        Log.d("FeedViewModel","searchMovies().success")
+                        _movieList.value = it.content
+                    }){
+                        Log.d("FeedViewModel","searchMovies().fail")
+                    }
+            )
+        }
+    }
+    /**영화 이름으로 리뷰 검색*/
+    fun searchReviews(title:String,sortType:Boolean,disposables: CompositeDisposable){
+        if(accessToken != null){
+            disposables.add(
+                MainService.searchReviews(accessToken,title,sortType)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        Log.d("FeedViewModel","searchReviews().success")
+                        _searchReviews.value = it
+                    }){
+                        Log.d("FeedViewModel","searchReviews().fail")
                     }
             )
         }
