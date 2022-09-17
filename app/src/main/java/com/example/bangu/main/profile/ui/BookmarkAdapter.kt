3424a32272fb0e21f54d2411b6a6cc9ce5e19fcc
Yoning Lee <0517lyi@naver.com>
2022-09-17 21:25:34 +0900

    Fix:"수정 - 바인딩 오류" bind함수가 겹쳐 있었음

--- app/src/main/java/com/example/bangu/main/profile/ui/BookmarkAdapter.kt
+++ app/src/main/java/com/example/bangu/main/profile/ui/BookmarkAdapter.kt
@@ -33,102 +33,99 @@ class BookmarkAdapter():RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             binding.defaultGenre3, binding.defaultGenre4, binding.defaultGenre5
         )
         fun bind(content: Content) {
-            fun bind(content: Content) {
-                /**북마크 추가, 삭제 기능*/
-                var sign: Boolean
-                val bookmark = binding.reviewBookmark
-                bookmark.apply {
-                    setOnClickListener {
-                        sign = bookmark.isSelected //상태 값 할당
-                        bookmark.isSelected = !sign
-                        viewmodel.adjustBookmark(content.id) //id를 넘겨줌
-                    }
+            /**북마크 추가, 삭제 기능*/
+            var sign: Boolean
+            val bookmark = binding.reviewBookmark
+            bookmark.apply {
+                setOnClickListener {
+                    sign = bookmark.isSelected //상태 값 할당
+                    bookmark.isSelected = !sign
+                    viewmodel.adjustBookmark(content.id) //id를 넘겨줌
                 }
-                /**북마크, 팔로우&팔로잉 상태 바인딩*/
-                if (content.followState) binding.followingBtn.isSelected = true
-                if (content.bookmark) binding.reviewBookmark.isSelected = true
+            }
+            /**북마크, 팔로우&팔로잉 상태 바인딩*/
+            if (content.followState) binding.followingBtn.isSelected = true
+            if (content.bookmark) binding.reviewBookmark.isSelected = true
 
-                /**팔로우&팔로잉 기능*/
-                var follow_sign: Boolean
-                binding.followingBtn.apply {
-                    setOnClickListener {
-                        follow_sign = it.isSelected
-                        it.isSelected = !follow_sign
-                        when (isSelected) {
-                            true -> _unfollow.postValue(Event(content.userProfileData!!.id)) //언팔로우 전달
-                            else -> _follow.postValue(Event(content.userProfileData!!.id)) //팔로우 전달
-                        }
+            /**팔로우&팔로잉 기능*/
+            var follow_sign: Boolean
+            binding.followingBtn.apply {
+                setOnClickListener {
+                    follow_sign = it.isSelected
+                    it.isSelected = !follow_sign
+                    when (isSelected) {
+                        true -> _unfollow.postValue(Event(content.userProfileData!!.id)) //언팔로우 전달
+                        else -> _follow.postValue(Event(content.userProfileData!!.id)) //팔로우 전달
                     }
                 }
-                /**리뷰를 작성한 계정 프로필 바인딩*/
-                binding.apply {
-                    userNickname.text = content.userProfileData?.nickname
-                    userGender.text = if (content.userProfileData?.gender == "M") "남성" else "여성"
-                    userAgearea.text = when (content.userProfileData?.birth) {
-                        1 -> "10대"
-                        2 -> "20대"
-                        3 -> "30대"
-                        4 -> "40대"
-                        5 -> "50대"
-                        else -> "60대 이상"
-                    }
+            }
+            /**리뷰를 작성한 계정 프로필 바인딩*/
+            binding.apply {
+                userNickname.text = content.userProfileData?.nickname
+                userGender.text = if (content.userProfileData?.gender == "M") "남성" else "여성"
+                userAgearea.text = when (content.userProfileData?.birth) {
+                    1 -> "10대"
+                    2 -> "20대"
+                    3 -> "30대"
+                    4 -> "40대"
+                    5 -> "50대"
+                    else -> "60대 이상"
+                }
 
-                }
-                /**프로필 이미지 바인딩*/
-                val profile_imageUrl = content.userProfileData?.imageUrl
-                when (profile_imageUrl) {
-                    "" -> Glide.with(binding.root).load(R.drawable.review_icon)
-                        .override(Target.SIZE_ORIGINAL).into(binding.userImage)
-                    else -> Glide.with(binding.root).load(profile_imageUrl)
-                        .override(Target.SIZE_ORIGINAL).into(binding.userImage)
-                }
-                /**영화 및 리뷰 내용, 팔로잉, 북마크, 별점 바인딩*/
-                binding.apply {
-                    reviewMovietitle.text = content.movieResponseData?.title
-                    reviewCore.text = content.content
-                    reviewBookmark.isSelected = content.bookmark //영화 북마크 여부
-                    followingBtn.isSelected = content.followState
-                    reviewStars.rating = content.score
-                    /**영화 장르 해쉬 태그*/
-                    var genre_token = content.movieResponseData?.genre?.split(' ')
-                    if (genre_token != null) {
-                        for (i in 0..genre_token.size - 1) {
-                            genre_array[i].text = genre_token[i] //장르 문자열 삽입
-                            genre_array[i].visibility = View.VISIBLE //가시적이게 전환
-                        }
+            }
+            /**프로필 이미지 바인딩*/
+            val profile_imageUrl = content.userProfileData?.imageUrl
+            when (profile_imageUrl) {
+                "" -> Glide.with(binding.root).load(R.drawable.review_icon)
+                    .override(Target.SIZE_ORIGINAL).into(binding.userImage)
+                else -> Glide.with(binding.root).load(profile_imageUrl)
+                    .override(Target.SIZE_ORIGINAL).into(binding.userImage)
+            }
+            /**영화 및 리뷰 내용, 팔로잉, 북마크, 별점 바인딩*/
+            binding.apply {
+                reviewMovietitle.text = content.movieResponseData?.title
+                reviewCore.text = content.content
+                reviewBookmark.isSelected = content.bookmark //영화 북마크 여부
+                followingBtn.isSelected = content.followState
+                reviewStars.rating = content.score
+                /**영화 장르 해쉬 태그*/
+                var genre_token = content.movieResponseData?.genre?.split(' ')
+                if (genre_token != null) {
+                    for (i in 0..genre_token.size - 1) {
+                        genre_array[i].text = genre_token[i] //장르 문자열 삽입
+                        genre_array[i].visibility = View.VISIBLE //가시적이게 전환
                     }
                 }
-                /**영화 이미지 바인딩*/
-                val movie_imageUrl = content.movieResponseData?.imageUrl
-                when (movie_imageUrl) {
-                    " " -> Glide.with(binding.root).load(R.drawable.movie01)
-                        .override(Target.SIZE_ORIGINAL).into(binding.reviewMovieimage)
-                    null -> Glide.with(binding.root).load(R.drawable.movie03)
-                        .override(Target.SIZE_ORIGINAL).into(binding.reviewMovieimage)
-                    else -> Glide.with(binding.root).load(movie_imageUrl)
-                        .override(Target.SIZE_ORIGINAL).into(binding.reviewMovieimage)
-                }
-                /**OTT 바인딩*/
-                /**OTT 아이콘 초기화*/
-                binding.apply {
-                    netflix.visibility = View.GONE
-                    tving.visibility = View.GONE
-                    watcha.visibility = View.GONE
-                    wavve.visibility = View.GONE
-                }
-                val ottSize = content.movieResponseData?.movieOtts?.size
-                for (i in 0 until ottSize!!) {
-                    when (content.movieResponseData?.movieOtts!![i].ottName) {
-                        "NETFLIX" -> binding.netflix.visibility = View.VISIBLE
-                        "TVING" -> binding.tving.visibility = View.VISIBLE
-                        "WATCHAPLAY" -> binding.watcha.visibility = View.VISIBLE
-                        "WAVVE" -> binding.wavve.visibility = View.VISIBLE
-                    }
+            }
+            /**영화 이미지 바인딩*/
+            val movie_imageUrl = content.movieResponseData?.imageUrl
+            when (movie_imageUrl) {
+                " " -> Glide.with(binding.root).load(R.drawable.movie01)
+                    .override(Target.SIZE_ORIGINAL).into(binding.reviewMovieimage)
+                null -> Glide.with(binding.root).load(R.drawable.movie03)
+                    .override(Target.SIZE_ORIGINAL).into(binding.reviewMovieimage)
+                else -> Glide.with(binding.root).load(movie_imageUrl)
+                    .override(Target.SIZE_ORIGINAL).into(binding.reviewMovieimage)
+            }
+            /**OTT 바인딩*/
+            /**OTT 아이콘 초기화*/
+            binding.apply {
+                netflix.visibility = View.GONE
+                tving.visibility = View.GONE
+                watcha.visibility = View.GONE
+                wavve.visibility = View.GONE
+            }
+            val ottSize = content.movieResponseData?.movieOtts?.size
+            for (i in 0 until ottSize!!) {
+                when (content.movieResponseData?.movieOtts!![i].ottName) {
+                    "NETFLIX" -> binding.netflix.visibility = View.VISIBLE
+                    "TVING" -> binding.tving.visibility = View.VISIBLE
+                    "WATCHAPLAY" -> binding.watcha.visibility = View.VISIBLE
+                    "WAVVE" -> binding.wavve.visibility = View.VISIBLE
                 }
             }
         }
     }
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
